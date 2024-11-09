5b152f830ef498c596d575e1720354b8951f6a52
vikas <vksmishra717@gmail.com>
2024-11-10 04:08:54 +0530

    2nd  rg added

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "RG1" {
     name="RG_VIKAS"
     location = "central india"  
 }
+
+resource "azurerm_resource_group" "RG2" {
+    name="RG_VIKAS"
+    location = "south india"  
+}
